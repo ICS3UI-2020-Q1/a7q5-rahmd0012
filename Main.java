1d5429f8a8bf5de113943430903cf636ef248ec8
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-13 17:07:44 +0000

    asks the user for a number and tells them the first digit

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that gets the first digit from a user entered number
+ * @author Daniel Rahmani
  */
 public class Main {
+
+  public static int firstDigit(int digit){
+    do{
+      // divide by 10 until theres 1 number
+      digit = digit / 10;
+    }while(digit >= 10);
+    int firstDigit = digit % 10;
+    return firstDigit;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a number
+    System.out.println("Please enter a number");
+    int n = input.nextInt();
+    // calculate the first digit
+    int answer = firstDigit(n);
+    System.out.println(answer);
     
   }
 }
